d9cc4477caa186369fd6ccbf78241dc9d72d6af8
wzrddd <d.kalynychenko@gmail.com>
2026-03-12 19:58:17 +0200

    Add web project to the solution

---
+++ src/LaconicAndIconic.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LaconicAndIconic.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
