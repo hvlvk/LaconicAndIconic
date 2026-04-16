fe5bd488413ad97e7b835f46b8162f14c06223b2
KAaYRAa <oksankatymo@gmail.com>
2026-04-17 01:04:49 +0300

    feat: complete category service implementation

--- src/LaconicAndIconic.BLL/Interfaces/ICategoryService.cs
+++ src/LaconicAndIconic.BLL/Interfaces/ICategoryService.cs
@@ -5,4 +5,8 @@ namespace LaconicAndIconic.BLL.Interfaces;
 public interface ICategoryService
 {
     Task<Result<IEnumerable<CategoryDto>>> GetAllCategoriesAsync();
+    Task<Result<IEnumerable<CategoryDto>>> GetAllAsync();
+    Task<Result> CreateAsync(string name);
+    Task<Result> UpdateAsync(int id, string name);
+    Task<Result> DeleteAsync(int id);
 }
